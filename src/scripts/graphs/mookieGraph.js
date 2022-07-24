7dfdf771cd475a70a64123de7a1d2f72af36eecc
Calvin Koo <kkoo22@students.claremontmckenna.edu>
2022-07-24 18:47:31 -0400

    graph interactivity grouping

--- src/scripts/graphs/mookieGraph.js
+++ src/scripts/graphs/mookieGraph.js
@@ -13,7 +13,68 @@ window.addEventListener('DOMContentLoaded', e => {
     let height = 500;
     let padding = 50;
     let margin = {left: 30, top: 80, bottom: 30, right: 20};
-    
+
+    //Grouping Logic
+    const highlight = function(event) {
+
+        //grab the identifier for which group we want
+        let hitType = event.getAttribute('data-event');
+
+        //hard coding the color value that we want
+        let color = '';
+        if (hitType === 'single') {
+            color = 'red';
+        } else if (hitType === 'double') {
+            color = 'yellow';
+        } else if (hitType === 'triple') {
+            color = 'green';
+        } else {
+            color = 'blue';
+        }
+
+        //fill all the other circles with a generic color and smaller size
+        d3.selectAll(".dot")
+            .transition()
+            .duration(200)
+            .style("fill", "lightgrey")
+            .attr("r", 3)
+        
+        //adjust the same group circles
+
+        d3.selectAll('.' + hitType)
+            .transition()
+            .duration(200)
+            .style("fill", color)
+            .attr("r", 8)
+    }
+
+     // Highlight the specie that is hovered
+    const doNotHighlight = function(){
+        d3.selectAll('.single')
+            .transition()
+            .duration(200)
+            .style("fill", 'red')
+            .attr("r", 5 )
+
+        d3.selectAll('.double')
+            .transition()
+            .duration(200)
+            .style("fill", 'yellow')
+            .attr("r", 5 )
+        
+        d3.selectAll('.triple')
+            .transition()
+            .duration(200)
+            .style("fill", 'green')
+            .attr("r", 5 )
+            
+        d3.selectAll('.home_run')
+            .transition()
+            .duration(200)
+            .style("fill", 'blue')
+            .attr("r", 5 )
+    }
+        
     let svg = d3.select('#mookie');
 
     let drawCanvas = () => {
@@ -44,14 +105,16 @@ window.addEventListener('DOMContentLoaded', e => {
         .style('background-color', 'lightblue')
         .style('border', 'solid')
         .style('border-width', '2px')
-        .style('opacity', 1)
+        .style('opacity', 0.75)
 
     let drawPoint = () => {
         svg.selectAll('circle')
             .data(values)
             .enter()
             .append('circle')
-            .attr('class', 'dot')
+            .attr('class', (item) => {
+                return 'dot' + ' ' + item['events']
+            })
             .attr('r', '5')
             .attr('data-xvalue', (item) => {
                 return item['launch_speed'];
@@ -70,6 +133,9 @@ window.addEventListener('DOMContentLoaded', e => {
             })
             .attr('data-description', (item) => {
                 return item['des']
+            })
+            .attr('data-event', (item) => {
+                return item['events']
             })
             .attr('cx', (item) => {
                 return xScale(item['launch_speed'])
@@ -98,13 +164,16 @@ window.addEventListener('DOMContentLoaded', e => {
                     .style('font-family', 'sans-serif')
                 .html()
                 d3.select(this)
-                    .style('stroke', 'black')   
+                    .style('stroke', 'black')
+                highlight(this)  
             })
             .on('mouseleave', function() {
                 tooltip.transition()
                     .style('visibility', 'hidden')
                 d3.select(this)
                     .style('stroke', 'none')
+                    .style('stroke-width', 5)
+                doNotHighlight()
             })
     }
 
