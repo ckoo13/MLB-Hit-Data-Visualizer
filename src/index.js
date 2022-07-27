99e92a5debd187fabe6461db0a1f8fe4a2681ec6
Calvin Koo <kkoo22@students.claremontmckenna.edu>
2022-07-27 07:20:07 -0400

    dryed out graph logic on main

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
-//Loading in all the graphs
-const mookieGraph = require('./scripts/graphs/mookieGraph.js');
-const tatisGraph = require('./scripts/graphs/tatisGraph.js');
-const troutGraph = require('./scripts/graphs/troutGraph.js');
-const seagerGraph = require('./scripts/graphs/seagerGraph.js');
-const machadoGraph = require('./scripts/graphs/machadoGraph.js');
-const harperGraph = require('./scripts/graphs/harperGraph.js');
-const acunaGraph = require('./scripts/graphs/acunaGraph.js');
-const altuveGraph = require('./scripts/graphs/altuveGraph.js');
-const freemanGraph = require('./scripts/graphs/freemanGraph.js');
-const jramGraph = require('./scripts/graphs/jramGraph.js');
-const jtGraph = require('./scripts/graphs/jtGraph.js');
-const judgeGraph = require('./scripts/graphs/judgeGraph.js');
-const ohtaniGraph = require('./scripts/graphs/ohtaniGraph.js');
-const sotoGraph = require('./scripts/graphs/sotoGraph.js');
-const vottoGraph = require('./scripts/graphs/vottoGraph.js');
+//Load in one graph
+const graph = require('./scripts/graph.js');
 
 //Loading in Intersection Observer API
 const observer = require('./scripts/observer.js');
