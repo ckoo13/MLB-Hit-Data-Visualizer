ac62fa57a16c0290cdeb36a298ec713d385930f0
ckoo13 <kyungmkoo13@gmail.com>
2022-10-20 14:51:40 -0700

    creating class for mediascroller

---
+++ src/scripts/mediascroller.js
@@ -0,0 +1,8 @@
+export default class MediaScroller {
+    constructor() {
+        // Create list of player names that we need to add as the href
+        this.players = ['mookie', 'tatis', 'trout', 'seager', 'machado', 'harper', 'acuna', 'altuve', 'freeman', 'jram', 'judge', 'ohtani', 'soto'];
+
+        
+    }
+}
